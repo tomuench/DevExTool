96be0cb9ac4ac5c840eaf29243f5117601c98394
Tobias Münch <to.muench@muench-its.de>
2024-11-02 13:45:48 +0100

    workflow integration

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -7,11 +7,13 @@ class Workflow < ApplicationRecord
   # Generating every workflow step for a new user
   def create_workflow_steps
     # Create a new workflow step for every step in the workflow
-    self.workflow_steps.create(
+    step1 = self.workflow_steps.create(
       position: 1,
       title: 'Demographic and skill level',
-      description: 'Survey about demographics and skills'
+      description: 'Survey about demographics and skills',
+      survey_ids: [4]
     )
+
     self.workflow_steps.create(
       position: 2,
       title: 'Task 1',
--- app/models/workflow_step.rb
+++ app/models/workflow_step.rb
@@ -1,2 +1,19 @@
 class WorkflowStep < ApplicationRecord
+
+  belongs_to :workflow
+  has_many :surveys, class_name: 'Rapidfire::Attempt', foreign_key: 'workflow_step_id'
+
+  attr_accessor :survey_ids
+
+  after_create :create_surveys
+
+
+  def create_surveys
+    if survey_ids
+      survey_ids.each do |survey_id|
+        survey = Rapidfire::Survey.find(survey_id)
+        surveys.create(survey_id: survey_id, user: workflow.user)
+      end
+    end
+  end
 end
